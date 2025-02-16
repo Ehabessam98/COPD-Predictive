0513d669c74d91e94b933bc8e2ee9a8542561a7c
Ehabessam98 <ee1638564@gmail.com>
2025-02-16 23:06:35 +0200

    COPD_app.py

--- COPD_app.py
+++ COPD_app.py
@@ -56,7 +56,7 @@ st.plotly_chart(fig)
 st.markdown(
     """
     <div style="text-align: center; margin-top: 20px; font-size: 16px; font-weight: bold;">
-        🚀 Created By: <span style="color: grey;">Ehab Essam</span> 🚀
+         Created By: <span style="color: grey;">Ehab Essam</span> 
     </div>
     """,
     unsafe_allow_html=True
